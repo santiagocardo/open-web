347d8cb1790fff25cb671a2decea60a9a6ab250a
Santiago Cardona <santiagocardo80@gmail.com>
2019-12-30 13:24:18 -0500

    Add events page

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/eventos', (req, res) => {
+  res.render('events')
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port http://localhost:3000`)
 })
